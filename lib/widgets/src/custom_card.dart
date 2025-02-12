1f4b7484bc23f0dd9d4191b4d1eb7b108cfb0439
Alvin Anum <nivla360c@gmail.com>
2025-02-12 12:42:51 +0000

    Update custom_card.dart

--- lib/widgets/src/custom_card.dart
+++ lib/widgets/src/custom_card.dart
@@ -6,14 +6,15 @@ class CustomCard extends StatelessWidget {
   final BoxShadow? shadow;
   final double borderRadius;
   final VoidCallback? onTap;
+  final Color? color;
 
-  const CustomCard({required this.child, this.borderRadius = 15, this.onTap,this.shadow, super.key});
+  const CustomCard({required this.child, this.color, this.borderRadius = 15, this.onTap,this.shadow, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
-          color: Theme.of(context).scaffoldBackgroundColor,
+          color: color ?? Theme.of(context).scaffoldBackgroundColor,
           boxShadow: [
             shadow ?? BoxShadow(
               offset: const Offset(0, 0),

61b0c0ee716e7165797237e17e7462a89176213a
Alvin Anum <nivla360c@gmail.com>
2025-10-20 12:17:23 +0000

    Refactor ErrorView constructor parameters

--- lib/widgets/src/error_view.dart
+++ lib/widgets/src/error_view.dart
@@ -5,7 +5,6 @@ import 'package:ionicons/ionicons.dart';
 
 import 'platform_image_asset.dart';
 
-
 enum _ErrorType { noInternet, generalError, noResults, onLoadMoreError }
 
 String? _image, _title, _subtitle;
@@ -14,7 +13,7 @@ class ErrorView extends StatelessWidget {
   final _ErrorType _errorType;
   final VoidCallback? onRetry;
   final bool isChat;
-  final String? imageAsset,title,subtitle,retryButtonLabel;
+  final String? imageAsset, title, subtitle, retryButtonLabel;
   final IconData? icon;
   final Color? iconColor;
   final double imageSize;
@@ -24,9 +23,12 @@ class ErrorView extends StatelessWidget {
       this.onRetry,
       this.imageSize = 250,
       this.isChat = false,
-        this.retryButtonLabel = 'Retry',
-  this.title,this.subtitle,
-      this.imageAsset, this.icon, this.iconColor})
+      this.retryButtonLabel = 'Retry',
+      this.title,
+      this.subtitle,
+      this.imageAsset,
+      this.icon,
+      this.iconColor})
       : _errorType = _ErrorType.generalError,
         super(key: key);
 
@@ -35,34 +37,41 @@ class ErrorView extends StatelessWidget {
       this.onRetry,
       this.imageSize = 250,
       this.isChat = false,
-        this.retryButtonLabel = 'Retry',
-        this.title,this.subtitle,
-        this.icon,
-        this.iconColor,
+      this.retryButtonLabel = 'Retry',
+      this.title,
+      this.subtitle,
+      this.icon,
+      this.iconColor,
       this.imageAsset})
       : _errorType = _ErrorType.noInternet,
         super(key: key);
 
-  const ErrorView.noResult(
-      {Key? key,
-      this.onRetry,
-      this.imageSize = 250,
-      this.isChat = false,
-        this.title,this.subtitle,
-        this.retryButtonLabel = 'Retry',
-      this.imageAsset, this.icon, this.iconColor,})
-      : _errorType = _ErrorType.noResults,
+  const ErrorView.noResult({
+    Key? key,
+    this.onRetry,
+    this.imageSize = 250,
+    this.isChat = false,
+    this.title,
+    this.subtitle,
+    this.retryButtonLabel = 'Retry',
+    this.imageAsset,
+    this.icon,
+    this.iconColor,
+  })  : _errorType = _ErrorType.noResults,
         super(key: key);
 
-  const ErrorView.onLoadMoreError(
-      {Key? key,
-      this.onRetry,
-      this.imageSize = 250,
-      this.isChat = false,
-        this.title,this.subtitle,
-        this.retryButtonLabel = 'Retry',
-      this.imageAsset, this.icon, this.iconColor,})
-      : _errorType = _ErrorType.onLoadMoreError,
+  const ErrorView.onLoadMoreError({
+    Key? key,
+    this.onRetry,
+    this.imageSize = 250,
+    this.isChat = false,
+    this.title,
+    this.subtitle,
+    this.retryButtonLabel = 'Retry',
+    this.imageAsset,
+    this.icon,
+    this.iconColor,
+  })  : _errorType = _ErrorType.onLoadMoreError,
         super(key: key);
 
   @override
@@ -104,17 +113,27 @@ class ErrorView extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                icon != null ? Container(
-                  decoration: BoxDecoration(
-                    color: iconColor?.withValues(alpha: 0.1) ?? Colors.grey.shade100,
-                    borderRadius: BorderRadius.circular(imageSize / 2),
-                  ),
-                  child: Icon(icon, size: imageSize,color: iconColor ?? context.theme.primaryColor,),).paddingOnly(bottom: 16) : PlatformImageAsset(
-                    imageAsset ?? _image ?? imagesErrorPath,//Assets.images.error.path,
-                    width: imageSize,
-                    height: imageSize / 1.2,
-                    fit: BoxFit.cover,
-                  ).clipAllCorners(10),
+                  icon != null
+                      ? Container(
+                          decoration: BoxDecoration(
+                            color: iconColor?.withValues(alpha: 0.1) ??
+                                Colors.grey.shade100,
+                            borderRadius: BorderRadius.circular(imageSize / 2),
+                          ),
+                          child: Icon(
+                            icon,
+                            size: imageSize,
+                            color: iconColor ?? context.theme.primaryColor,
+                          ).paddingAll(16),
+                        ).paddingOnly(bottom: 16)
+                      : PlatformImageAsset(
+                          imageAsset ??
+                              _image ??
+                              imagesErrorPath, //Assets.images.error.path,
+                          width: imageSize,
+                          height: imageSize / 1.2,
+                          fit: BoxFit.cover,
+                        ).clipAllCorners(10),
                   // verticalSpaceTen,
                   Text(
                     _title ?? '',
@@ -122,7 +141,10 @@ class ErrorView extends StatelessWidget {
                     style: context.theme.appBarTheme.titleTextStyle!
                         .copyWith(fontSize: 25),
                   ),
-                  Text(_subtitle ?? '',textAlign: TextAlign.center,),
+                  Text(
+                    _subtitle ?? '',
+                    textAlign: TextAlign.center,
+                  ),
                   if (onRetry != null) ...[
                     verticalSpaceTen,
                     TextButton(
@@ -145,22 +167,22 @@ class ErrorView extends StatelessWidget {
   _init() {
     switch (_errorType) {
       case _ErrorType.noInternet:
-        _image =  imagesNoInternetPath;//Assets.images.noInternet.path;
+        _image = imagesNoInternetPath; //Assets.images.noInternet.path;
         _title = title ?? 'Connection Lost';
         _subtitle = subtitle ?? 'Please make sure you have internet connection';
         break;
       case _ErrorType.generalError:
-        _image = imagesErrorPath;//Assets.images.error.path;
-        _title = title ??'Oops...';
+        _image = imagesErrorPath; //Assets.images.error.path;
+        _title = title ?? 'Oops...';
         _subtitle = subtitle ?? 'That was unexpected, please try again later';
         break;
       case _ErrorType.noResults:
-        _image = imagesNoResultsPath;//Assets.images.empty.path;
+        _image = imagesNoResultsPath; //Assets.images.empty.path;
         _title = title ?? 'Nothing found';
         _subtitle = subtitle ?? 'Sorry, nothing to show here';
         break;
       case _ErrorType.onLoadMoreError:
-        _image = imagesErrorPath;//Assets.images.error.path;
+        _image = imagesErrorPath; //Assets.images.error.path;
         _title = title ?? '';
         _subtitle = subtitle ?? 'An error occurred';
         break;
